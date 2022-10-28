1c1d9a4f589e4450dde2d62a45bc90b69f115fc6
Artemiy <shuckinart@yandex.ru>
2022-10-28 15:18:46 +0000

    woo

--- src/index.ts
+++ src/index.ts
@@ -4,19 +4,50 @@ import { readFile, writeFile, open } from 'node:fs/promises'
 type ValueOr<T> = T extends T & Function ? never : T | (() => T)
 
 const unwrap = <T>(val: ValueOr<T>): T => {
-  return typeof val === 'function' ? val() : val;
+	return typeof val === 'function' ? val() : val
+}
+
+interface PatchyyFindTarget {
+	/**
+	 * Найти, и заменить
+	 */
+	readonly find: ValueOr<string | RegExp>
+	readonly patch: (match: string) => string
 }
 
 interface PatchyyLineTarget {
-  readonly at: ValueOr<number>;
-  readonly patch: (line: string) => string;
+	/**
+	 * Строка, в которой будет произведена замена
+	 */
+	readonly at: ValueOr<number>
+	readonly patch: (line: string) => string
+}
+
+interface PatchyyRangeTarget {
+	/**
+	 * Диапазон
+	 */
+	readonly range: [number, number]
+	/**
+	 * Диапазон в строках
+	 */
+	readonly line?: boolean
+	readonly patch: (str: string) => string
 }
 
 interface PatchyyTarget {
+	/**
+	 * Файл, в котором будут произведены операции изменения
+	 */
 	readonly id: string
 }
 
-type PatchyyConfig = readonly (PatchyyTarget & PatchyyLineTarget)[]
+type PatchyyConfigModifiers =
+	| PatchyyLineTarget
+	| PatchyyFindTarget
+	| PatchyyRangeTarget
+
+type PatchyyConfig = readonly (PatchyyTarget & PatchyyConfigModifiers)[]
 
 const Patchyy = class {
 	private config: PatchyyConfig
@@ -26,42 +57,82 @@ const Patchyy = class {
 	}
 
 	public resolve(id: string): string {
-    return resolve('./node_modules', id);
-  }
+		return resolve('./node_modules', id)
+	}
 
-  public async patch() {
-    for (const target of this.config) {
-      const id = this.resolve(target.id);
-      // @todo: open file
-      const source = await readFile(id, 'utf-8')
+	public async patch() {
+		for (const target of this.config) {
+			const id = this.resolve(target.id)
+			// @todo: open file
+			const source = await readFile(id, 'utf-8')
 
-      let value = '';
+			let value = ''
 
-      if ('at' in target) {
-        const at = unwrap(target.at);
+			if ('at' in target) {
+				const index = unwrap(target.at) - 1
+				const lines = source.split('\n')
 
-        const splitted = source.split('\n');
+				lines[index] = target.patch(lines[index])
 
-        splitted[at - 1] = target.patch(splitted[at - 1]);
+				value = lines.join('\n')
+			} else if ('find' in target) {
+				value = source.replace(unwrap(target.find), target.patch)
+			} else if ('range' in target) {
+				const [start, end] = target.range
 
-        value = splitted.join('\n');
-      }
+				const separator = target.line ? '\n' : ''
 
-      await writeFile(id, value, 'utf-8');
-    }
-  }
+				const lines = source.split(separator)
+				const content = lines.slice(start - 1, end).join(separator)
+
+				const patched = target.patch(content)
+
+				const part0 = lines.slice(0, start - 1).join(separator)
+				const part1 = lines.slice(end, lines.length).join(separator)
+
+				value = part0 + separator + patched + separator + part1
+			}
+
+			await writeFile(id, value, 'utf-8')
+		}
+	}
 }
 
 const patchyy = new Patchyy([
-  {
-    id: '@babel/core/lib/index.js',
-    at: 250,
-    patch(line) {
-      console.log(line);
+	{
+		id: '@babel/core/lib/config/config-descriptors.js',
+		at: 61,
+		patch(line) {
+			return line.replace(
+				'optionsWithResolvedBrowserslistConfigFile(options, dirname)',
+				'options'
+			)
+		},
+	},
+	{
+		id: '@babel/core/lib/config/config-chain.js',
+		find() {
+			return '(babelrc === true || babelrc === undefined) && typeof context.filename === "string"'
+		},
+		patch() {
+			return 'false'
+		},
+	},
+	{
+		id: '@babel/core/lib/config/helpers/config-api.js',
+		range: [75, 104],
+		line: true,
+		patch() {
+			return ''
+		},
+	},
+	{
+		id: '@babel/core/lib/config/helpers/config-api.js',
+		range: [1, 14],
+		patch() {
+			return ''
+		},
+	},
+])
 
-      return line.replace('7.19.6', '8.0.0')
-    }
-  },
-]);
-
-patchyy.patch();
+patchyy.patch()
